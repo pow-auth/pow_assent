12a56db9b4705be665a846692507400d1187412a
Dan Schultzer <dan@dreamconception.com>
2019-01-11 13:34:06 -0800

    Refactor handling of Oauth/Oauth2 macros

--- lib/pow_assent/strategies/oauth2/base.ex
+++ lib/pow_assent/strategies/oauth2/base.ex
@@ -23,6 +23,7 @@ defmodule PowAssent.Strategy.OAuth2.Base do
         end
       end
   """
+  alias PowAssent.Strategy, as: Helpers
   alias PowAssent.Strategy.OAuth2
   alias Plug.Conn
 
@@ -38,43 +39,47 @@ defmodule PowAssent.Strategy.OAuth2.Base do
       use PowAssent.Strategy
 
       alias PowAssent.Strategy, as: Helpers
-      alias Plug.Conn
 
       @spec authorize_url(Keyword.t(), Conn.t()) :: {:ok, %{conn: Conn.t(), state: binary(), url: binary()}}
-      def authorize_url(config, conn) do
-        config
-        |> set_config()
-        |> OAuth2.authorize_url(conn)
-      end
+      def authorize_url(config, conn), do: unquote(__MODULE__).authorize_url(config, conn, __MODULE__)
 
       @spec callback(Keyword.t(), Conn.t(), map()) :: {:ok, %{conn: Conn.t(), user: map()}} | {:error, %{conn: Conn.t(), error: any()}}
-      def callback(config, conn, params) do
-        config = set_config(config)
+      def callback(config, conn, params), do: unquote(__MODULE__).callback(config, conn, params, __MODULE__)
 
-        config
-        |> OAuth2.callback(conn, params, __MODULE__)
-        |> maybe_normalize(config)
-      end
-
-      @spec get_user(Keyword.t(), map()) :: {:ok, map()} | {:error, any()}
       def get_user(config, token), do: OAuth2.get_user(config, token)
-
-      defp maybe_normalize({:ok, %{user: user} = results}, config) do
-        case normalize(config, user) do
-          {:ok, user}     -> {:ok, %{results | user: Helpers.prune(user)}}
-          {:error, error} -> maybe_normalize({:error, error}, config)
-        end
-      end
-      defp maybe_normalize({:error, error}, _config), do: {:error, error}
-
-      defp set_config(config) do
-        config
-        |> default_config()
-        |> Keyword.merge(config)
-        |> Keyword.put(:strategy, __MODULE__)
-      end
 
       defoverridable unquote(__MODULE__)
     end
+  end
+
+  @spec authorize_url(Keyword.t(), Conn.t(), module()) :: {:ok, %{conn: Conn.t(), state: binary(), url: binary()}}
+  def authorize_url(config, conn, strategy) do
+    config
+    |> set_config(strategy)
+    |> OAuth2.authorize_url(conn)
+  end
+
+  @spec callback(Keyword.t(), Conn.t(), map(), module()) :: {:ok, %{conn: Conn.t(), user: map()}} | {:error, %{conn: Conn.t(), error: any()}}
+  def callback(config, conn, params, strategy) do
+    config = set_config(config, strategy)
+
+    config
+    |> OAuth2.callback(conn, params, strategy)
+    |> normalize(config, strategy)
+  end
+
+  defp normalize({:ok, %{user: user} = results}, config, strategy) do
+    case strategy.normalize(config, user) do
+      {:ok, user}     -> {:ok, %{results | user: Helpers.prune(user)}}
+      {:error, error} -> normalize({:error, error}, config, strategy)
+    end
+  end
+  defp normalize({:error, error}, _config, _strategy), do: {:error, error}
+
+  defp set_config(config, strategy) do
+    config
+    |> strategy.default_config()
+    |> Keyword.merge(config)
+    |> Keyword.put(:strategy, strategy)
   end
 end
