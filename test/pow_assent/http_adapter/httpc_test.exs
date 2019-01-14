f50a2d162963dafb3b7612682d35f63077946add
Dan Schultzer <dan@dreamconception.com>
2019-01-14 11:23:26 -0800

    Prevent cache issue in test

--- test/pow_assent/http_adapter/httpc_test.exs
+++ test/pow_assent/http_adapter/httpc_test.exs
@@ -8,13 +8,11 @@ defmodule PowAssent.HTTPAdapter.HttpcTest do
   @hsts_certificate_url "https://hsts.badssl.com"
 
   describe "request/4" do
-    test "without SSL" do
-      assert {:ok, %HTTPResponse{status: 200}} = Httpc.request(:get, @expired_certificate_url, nil, [], [])
-    end
-
-    test "with SSL" do
+    test "handles SSL" do
       assert {:ok, %HTTPResponse{status: 200}} = Httpc.request(:get, @hsts_certificate_url, nil, [])
       assert {:error, :econnrefused} = Httpc.request(:get, @expired_certificate_url, nil, [])
+
+      assert {:ok, %HTTPResponse{status: 200}} = Httpc.request(:get, @expired_certificate_url, nil, [], [])
     end
   end
 end
