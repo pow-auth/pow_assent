c9df2638aefb07615c93242d13a4f2d9a4313bab
Dan Schultzer <dan@dreamconception.com>
2019-05-19 07:00:24 -0700

    Refactor mock

--- test/support/user_identities_mock.ex
+++ test/support/user_identities_mock.ex
@@ -3,31 +3,34 @@ defmodule PowAssent.Test.UserIdentitiesMock do
   use PowAssent.Ecto.UserIdentities.Context
   alias PowAssent.Test.Ecto.{UserIdentities.UserIdentity, Users.User}
 
+  @provider "test_provider"
+
   @user %User{id: 1, email: "test@example.com", password_hash: ""}
-  @changeset_invalid_name %User{} |> Ecto.Changeset.cast(%{"name" => ""}, [:name]) |> Ecto.Changeset.validate_required([:name]) |> Map.put(:action, :create)
-  @changeset_taken_email %User{} |> Ecto.Changeset.cast(%{"email" => "taken@example.com"}, [:email]) |> Ecto.Changeset.add_error(:email, "has already been taken") |> Map.put(:action, :create)
-  @user_identity %UserIdentity{provider: "test_provider", uid: "1"}
+  @user_identity %UserIdentity{provider: @provider, uid: "1"}
+
+  @new_user_identity_params %{"provider" => @provider, "uid" => "new_user"}
+  @taken_user_identity_params %{"provider" => @provider, "uid" => "identity_taken"}
+
   @created_user_with_identity %{@user | id: :new_user, user_identities: [%{@user_identity | uid: "new_user"}]}
+  @taken_user_id_changeset Ecto.Changeset.add_error(Ecto.Changeset.change(%User{}), :email, "has already been taken")
 
-  def get_user_by_provider_uid("test_provider", "existing_user"), do: @user
-  def get_user_by_provider_uid("test_provider", "new_user"), do: nil
+  def get_user_by_provider_uid(@provider, "existing_user"), do: @user
+  def get_user_by_provider_uid(@provider, "new_user"), do: nil
 
-  def create(_user, %{"provider" => "test_provider", "uid" => "new_identity"}), do: {:ok, %UserIdentity{id: :new_identity}}
-  def create(_user, %{"provider" => "test_provider", "uid" => "identity_taken"}), do: {:error, {:bound_to_different_user, Ecto.Changeset.change(%UserIdentity{})}}
+  def create(_user, %{"provider" => @provider, "uid" => "new_identity"}), do: {:ok, %{@user_identity | id: :new_identity}}
+  def create(user, @taken_user_identity_params), do: {:error, {:bound_to_different_user, %{User.changeset(user, @taken_user_identity_params) | action: :create}}}
 
-  @new_user_user_identity_params %{"provider" => "test_provider", "uid" => "new_user"}
+  def create_user(@taken_user_identity_params, params, _user_id_params), do: {:error, {:bound_to_different_user, UserIdentity.changeset(%UserIdentity{}, params)}}
+  def create_user(@new_user_identity_params, %{"name" => ""} = params, _user_id_params), do: {:error, %{User.changeset(%User{}, params) | action: :create}}
+  def create_user(@new_user_identity_params, %{"email" => ""} = params, _user_id_params), do: {:error, {:invalid_user_id_field, %{User.changeset(%User{}, params) | action: :create}}}
+  def create_user(@new_user_identity_params, %{"email" => "taken@example.com"} = params, _user_id_params), do: {:error, {:invalid_user_id_field, %{User.changeset(@taken_user_id_changeset, params) | action: :create}}}
+  def create_user(@new_user_identity_params, _params, %{"email" => "taken@example.com"} = user_id_params), do: {:error, {:invalid_user_id_field, %{User.changeset(@taken_user_id_changeset, user_id_params) | action: :create}}}
+  def create_user(@new_user_identity_params, _params, %{"email" => "foo@example.com"}), do: {:ok, %{@created_user_with_identity | email: "foo@example.com"}}
+  def create_user(@new_user_identity_params, _params, _user_id_params), do: {:ok, @created_user_with_identity}
 
-  def create_user(%{"provider" => "test_provider", "uid" => "identity_taken"}, _params, _user_id_params), do: {:error, {:bound_to_different_user, Ecto.Changeset.change(%UserIdentity{})}}
-  def create_user(@new_user_user_identity_params, %{"name" => ""}, _user_id_params), do: {:error, @changeset_invalid_name}
-  def create_user(@new_user_user_identity_params, %{"email" => ""}, _user_id_params), do: {:error, {:invalid_user_id_field, %{}}}
-  def create_user(@new_user_user_identity_params, %{"email" => "taken@example.com"}, _user_id_params), do: {:error, {:invalid_user_id_field, @changeset_taken_email}}
-  def create_user(@new_user_user_identity_params, _params, %{"email" => "foo@example.com"}), do: {:ok, %{@created_user_with_identity | email: "foo@example.com"}}
-  def create_user(@new_user_user_identity_params, _params, %{"email" => "taken@example.com"}), do: {:error, {:invalid_user_id_field, @changeset_taken_email}}
-  def create_user(@new_user_user_identity_params, _params, _user_id_params), do: {:ok, @created_user_with_identity}
-
-  def delete(%User{}, "test_provider"), do: {:ok, {1, nil}}
-  def delete(:error, "test_provider"), do: {:error, :error}
-  def delete(:no_password, "test_provider"), do: {:error, {:no_password, nil}}
+  def delete(%User{}, @provider), do: {:ok, {1, nil}}
+  def delete(:error, @provider), do: {:error, :error}
+  def delete(:no_password, @provider), do: {:error, {:no_password, nil}}
 
   def all(@user), do: [@user_identity]
 
