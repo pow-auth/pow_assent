8fcfb74054b055033fee23d8c380e8eca1895a98
Dan Schultzer <dan@dreamconception.com>
2019-06-04 12:42:29 -0700

    Handle SSL cert errors in mint test with otp 22

--- test/pow_assent/http_adapter/mint_test.exs
+++ test/pow_assent/http_adapter/mint_test.exs
@@ -8,10 +8,13 @@ defmodule PowAssent.HTTPAdapter.MintTest do
   @hsts_certificate_url "https://hsts.badssl.com"
   @unreachable_http_url "http://localhost:8888/"
 
+  {otp_version, _} = Integer.parse(to_string(:erlang.system_info(:otp_release)))
+  @certificate_expired_error if otp_version >= 22, do: {:tls_alert, {:certificate_expired, 'received CLIENT ALERT: Fatal - Certificate Expired'}}, else: {:tls_alert, 'certificate expired'}
+
   describe "request/4" do
     test "handles SSL" do
       assert {:ok, %HTTPResponse{status: 200}} = Mint.request(:get, @hsts_certificate_url, nil, [])
-      assert {:error, {:tls_alert, 'certificate expired'}} = Mint.request(:get, @expired_certificate_url, nil, [])
+      assert {:error, @certificate_expired_error} = Mint.request(:get, @expired_certificate_url, nil, [])
 
       assert {:ok, %HTTPResponse{status: 200}} = Mint.request(:get, @expired_certificate_url, nil, [], transport_opts: [verify: :verify_none])
 
